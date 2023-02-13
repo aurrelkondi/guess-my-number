d08edf377457a899b33e2c4ef179d07cb1095064
aurrelkondi <aurrelkondi@hotmail.com>
2023-02-13 16:46:25 +0100

    click events

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number🥳';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
-console.log((document.querySelector('.guess').value = 10));
+console.log((document.querySelector('.guess').value = 10)); */
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    console.log((document.querySelector('.message').textContent = 'No Number'));
+  }
+});
